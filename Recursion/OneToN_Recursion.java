4ab33bc3378602f4f92e89e8def9af7b9f46b45c
mhneloy <mhneloy708@gmail.com>
2026-06-22 14:06:27 +0600

    Print 1 to N using Recursion

--- Recursion/OneToN_Recursion.java
+++ Recursion/OneToN_Recursion.java
@@ -1,4 +1,15 @@
 package Recursion;
-
+import java.util.Scanner;
 public class OneToN_Recursion {
+    static void print1toN(int n){
+        if(n == 0)return;
+        print1toN(n-1);
+        System.out.println(n);
+    }
+    static void main() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter how many number you want to print from 1 to N:");
+        int n = sc.nextInt();
+        print1toN(n);
+    }
 }
