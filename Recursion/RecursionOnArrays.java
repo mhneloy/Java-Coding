cf8a83de138b630074738155540104641b6a6c33
mhneloy <mhneloy708@gmail.com>
2026-06-27 10:10:31 +0600

    find the element from array using recursion

--- Recursion/RecursionOnArrays.java
+++ Recursion/RecursionOnArrays.java
@@ -2,12 +2,20 @@ package Recursion;
 
 public class RecursionOnArrays {
     static void main() {
-        int[] arr = {5,3,7,12,76, 9, 34, 5};
+        int[] arr = {5,3,7,12,76, 9, 34, 6};//8
         recPrint(arr,0);
+        int target = 60;
+        System.out.println(exists(arr,target,0));
     }
     public static void recPrint(int[] arr, int idx){
         if(arr.length == idx) return;
         System.out.println(arr[idx]);
         recPrint(arr,idx+1);
+    }
+    public  static boolean exists(int[] arr,int target, int idx){
+        int len = arr.length;
+        if(len == idx) return false;
+        if(arr[idx] == target) return true;
+        return exists(arr,target,idx+1);
     }
 }
